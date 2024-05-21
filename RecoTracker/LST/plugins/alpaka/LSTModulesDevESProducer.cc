60d25fcaf85a918f9164909b390bf7984b30cc9b
Andres Rios Tascon <ariostas@gmail.com>
2024-05-21 17:21:43 -0400

    Match changes from TrackLooper repo

--- RecoTracker/LST/plugins/alpaka/LSTModulesDevESProducer.cc
+++ RecoTracker/LST/plugins/alpaka/LSTModulesDevESProducer.cc
@@ -14,42 +14,39 @@
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
-class LSTModulesDevESProducer : public ESProducer {
-public:
-  LSTModulesDevESProducer(const edm::ParameterSet &iConfig);
+  class LSTModulesDevESProducer : public ESProducer {
+  public:
+    LSTModulesDevESProducer(const edm::ParameterSet &iConfig);
 
-  static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+    static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
 
-  std::unique_ptr<SDL::LSTESHostData> produceHost(TrackerRecoGeometryRecord const& iRecord);
-  std::unique_ptr<SDL::LSTESDeviceData<SDL::Dev>> produceDevice(device::Record<TrackerRecoGeometryRecord> const& iRecord);
+    std::unique_ptr<SDL::LSTESHostData<SDL::Dev>> produceHost(TrackerRecoGeometryRecord const& iRecord);
+    std::unique_ptr<SDL::LSTESDeviceData<SDL::Dev>> produceDevice(device::Record<TrackerRecoGeometryRecord> const& iRecord);
 
-private:
-  edm::ESGetToken<SDL::LSTESHostData, TrackerRecoGeometryRecord> lstESHostToken_;
+  private:
+    edm::ESGetToken<SDL::LSTESHostData<SDL::Dev>, TrackerRecoGeometryRecord> lstESHostToken_;
+  };
 
-};
+  LSTModulesDevESProducer::LSTModulesDevESProducer(const edm::ParameterSet &iConfig) : ESProducer(iConfig) {
+    setWhatProduced(this, &LSTModulesDevESProducer::produceHost);
+    auto cc = setWhatProduced(this, &LSTModulesDevESProducer::produceDevice);
+    lstESHostToken_ = cc.consumes();
+  }
 
-LSTModulesDevESProducer::LSTModulesDevESProducer(const edm::ParameterSet &iConfig)
-  : ESProducer(iConfig)
-{
-  setWhatProduced(this, &LSTModulesDevESProducer::produceHost);
-  auto cc = setWhatProduced(this, &LSTModulesDevESProducer::produceDevice);
-  lstESHostToken_ = cc.consumes();
-}
+  void LSTModulesDevESProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
+    edm::ParameterSetDescription desc;
+    descriptions.addWithDefaultLabel(desc);
+  }
 
-void LSTModulesDevESProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
-  edm::ParameterSetDescription desc;
-  descriptions.addWithDefaultLabel(desc);
-}
+  std::unique_ptr<SDL::LSTESHostData<SDL::Dev>> LSTModulesDevESProducer::produceHost(TrackerRecoGeometryRecord const& iRecord) {
+    return SDL::loadAndFillESHost();
+  }
 
-std::unique_ptr<SDL::LSTESHostData> LSTModulesDevESProducer::produceHost(TrackerRecoGeometryRecord const& iRecord) {
-  return SDL::loadAndFillESHost();
-}
-
-std::unique_ptr<SDL::LSTESDeviceData<SDL::Dev>> LSTModulesDevESProducer::produceDevice(device::Record<TrackerRecoGeometryRecord> const& iRecord) {
-  auto const& lstESHostData = iRecord.get(lstESHostToken_);
-  SDL::QueueAcc& queue = iRecord.queue();
-  return SDL::loadAndFillESDevice(queue, &lstESHostData);
-}
+  std::unique_ptr<SDL::LSTESDeviceData<SDL::Dev>> LSTModulesDevESProducer::produceDevice(device::Record<TrackerRecoGeometryRecord> const& iRecord) {
+    auto const& lstESHostData = iRecord.get(lstESHostToken_);
+    SDL::QueueAcc& queue = iRecord.queue();
+    return SDL::loadAndFillESDevice(queue, &lstESHostData);
+  }
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE
 
